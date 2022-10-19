ea1b2e1656c2081fce893fbee6a7615ac09f9108
Taras <yogesh.bhatt@galaxyweblinks.in>
2022-10-20 01:07:17 +0530

    Print added

--- My WorK App/ViewController.swift
+++ My WorK App/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Login Screen")
         // Do any additional setup after loading the view.
     }
     
